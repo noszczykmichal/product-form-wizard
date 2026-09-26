0fd86e70732076840b5cbfd5fa950cbd53165f23
Michal <noszczykmichal@wp.pl>
2026-09-26 14:20:29 +0200

    fix suspense error

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ export default function Home() {
   return (
     <main className="flex w-full justify-center">
       <section className="py-6 lg:py-28 w-full px-4 flex flex-col items-center">
-        <Suspense fallback={<ProductCatalogSkeleton />}></Suspense>
-        <ProductCatalog />
+        <Suspense fallback={<ProductCatalogSkeleton />}>
+          <ProductCatalog />
+        </Suspense>
       </section>
     </main>
   );
