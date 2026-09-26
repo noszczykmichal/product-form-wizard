01847397f2e142ea8a16d1f4ab00a323820a8958
Michal <noszczykmichal@wp.pl>
2026-09-26 14:18:09 +0200

    add custom skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import ProductCatalog from "@/components/Products/ProductCatalog/ProductCatalog";
+import ProductCatalogSkeleton from "@/components/Products/ProductCatalogSkeleton/ProductCatalogSkeleton";
 
 export default function Home() {
   return (
     <main className="flex w-full justify-center">
       <section className="py-6 lg:py-28 w-full px-4 flex flex-col items-center">
+        <Suspense fallback={<ProductCatalogSkeleton />}></Suspense>
         <ProductCatalog />
       </section>
     </main>
